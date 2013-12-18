d8df1163f9419c9518702144722a6ff2365a7a18
Alan Liu <alanliumin@gmail.com>
2013-12-18 22:14:55 +0800

    Add user microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,13 +9,14 @@ class UsersController < ApplicationController
 
 	def show
 		@user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
 	end
 	
-  	def new
-  		@user = User.new
-  	end
+  def new
+  	@user = User.new
+  end
 
-  	def create
+  def create
 		@user = User.new(user_params)
 		if @user.save
 			sign_in @user
@@ -24,47 +25,40 @@ class UsersController < ApplicationController
 		else
 			render 'new'
 		end
-  	end
+  end
 
-  	def edit
-    	@user = User.find(params[:id])
-  	end
+  def edit
+    @user = User.find(params[:id])
+  end
 
-  	def update
-    	@user = User.find(params[:id])
-    	if @user.update_attributes(user_params)
-      		flash[:success] = "Profile updated"
-      		redirect_to @user
-    	else
-      		render 'edit'
-    	end
-  	end
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      	flash[:success] = "Profile updated"
+      	redirect_to @user
+    else
+      	render 'edit'
+    end
+  end
 
-    def destroy
-      User.find(params[:id]).destroy
-      flash[:success] = "User destroyed."
-      redirect_to users_url
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User destroyed."
+    redirect_to users_url
+  end
+
+  private
+    def user_params
+      	params.require(:user).permit(:name, :email, :password,
+                                   	 :password_confirmation)
     end
 
-  	private
-    	def user_params
-      		params.require(:user).permit(:name, :email, :password,
-                                   	 	 :password_confirmation)
-    	end
+    def correct_user
+      	@user = User.find(params[:id])
+      	redirect_to(root_path) unless current_user?(@user)
+    end
 
-    	def signed_in_user
-    		unless signed_in?
-    			store_location
-    			redirect_to signin_url, notice: "Please sign in."
-    		end
-    	end
-
-    	def correct_user
-      		@user = User.find(params[:id])
-      		redirect_to(root_path) unless current_user?(@user)
-    	end
-
-      def admin_user
-        redirect_to(root_path) unless current_user.admin?
-      end
+    def admin_user
+      redirect_to(root_path) unless current_user.admin?
+    end
 end
